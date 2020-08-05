fedf5b19690daae342cd4b3c4f3863e628c2b21b
Aral Roca Gomez <aral-rg@hotmail.com>
2020-08-05 18:01:11 +0200

    Refactor

--- src/useGears/index.js
+++ src/useGears/index.js
@@ -24,7 +24,6 @@ import { gears, rotationStep } from './constants';
  */
 export default function useGears() {
   const canvasRef = useRef();
-  const webglVars = useRef();
 
   // Initialize program with shaders + draw gears
   useEffect(init, []);
@@ -36,127 +35,108 @@ export default function useGears() {
     const rotationLocation = gl.getUniformLocation(program, 'u_rotation');
     const translationLocation = gl.getUniformLocation(program, 'u_translation');
     const moveOriginLocation = gl.getUniformLocation(program, 'u_moveOrigin');
+    const angles = Array.from({ length: gears.length }).map((v) => 0);
 
-    webglVars.current = {
-      angles: Array.from({ length: gears.length }).map((v) => 0),
-      gl,
-      program,
-      rotationLocation,
-      translationLocation,
-      moveOriginLocation,
-    };
+    /**
+     * Each piece of the gear has a similar structure: It is a circle,
+     * filled with color or only with the border.  So we'll use the same
+     * function for each piece.
+     *
+     * The only slightly different piece is the larger circle of the gear,
+     * as we will have to draw the teeth.
+     */
+    function renderGearPiece({
+      center,
+      radius,
+      fillColor,
+      strokeColor,
+      numberOfTeeth,
+    }) {
+      const { TRIANGLE_STRIP, POINTS, TRIANGLES } = gl;
+      const coords = getCircleCoords(gl, center[0], center[1], radius);
+
+      if (fillColor) drawShape(coords, fillColor, TRIANGLE_STRIP);
+      if (strokeColor) drawShape(coords, strokeColor, POINTS);
+      if (numberOfTeeth) {
+        drawShape(
+          getGearTeeth(gl, center, radius, numberOfTeeth),
+          fillColor,
+          TRIANGLES,
+        );
+      }
+    }
+
+    /**
+     * This function is in charge of drawing all the gears, giving it the
+     * rotation on itself that it touches according to the angle calculated
+     * in each interval.
+     */
+    function drawGears() {
+      gears.forEach((gear, index) => {
+        const [centerX, centerY] = gear.center;
+
+        gl.uniformMatrix3fv(
+          translationLocation,
+          false,
+          translation(centerX, centerY),
+        );
+        gl.uniformMatrix3fv(rotationLocation, false, rotation(angles[index]));
+        gl.uniformMatrix3fv(
+          moveOriginLocation,
+          false,
+          translation(-centerX, -centerY),
+        );
+        renderGearPiece(gear);
+        if (gear.children) gear.children.forEach(renderGearPiece);
+      });
+    }
+
+    /**
+     * Each piece of the gear, whether it is a toothed wheel, a painted
+     * circle, or a rimmed circle, has a different shape. So this function
+     * takes care of drawing each shape. Either a painted circle, which we
+     * will do with triangles, or an unpainted circle that we will do
+     * with points.
+     */
+    function drawShape(coords, color, drawingMode) {
+      const data = new Float32Array(coords);
+      const buffer = createAndBindBuffer(
+        gl,
+        gl.ARRAY_BUFFER,
+        gl.STATIC_DRAW,
+        data,
+      );
+
+      gl.useProgram(program);
+      linkGPUAndCPU(gl, { program, buffer, gpuVariable: 'position' });
+
+      const inputColor = gl.getUniformLocation(program, 'inputColor');
+      gl.uniform3fv(inputColor, color);
+      gl.drawArrays(drawingMode, 0, coords.length / 2);
+    }
+
+    /**
+     * This function is in charge of rendering each frame. When it is finished,
+     * it calls itself again.
+     */
+    function run() {
+      gears.forEach((gear, index) => {
+        const direction = gear.direction === 'clockwise' ? 1 : -1;
+        const step = direction * (rotationStep / gear.numberOfTeeth);
+
+        angles[index] = (angles[index] + step) % 360;
+      });
+      drawGears();
+
+      // Render next frame
+      window.requestAnimationFrame(run);
+    }
 
     // Start rendering
-    run()
+    run();
   }
 
-  /**
-   * Each piece of the gear has a similar structure: It is a circle, 
-   * filled with color or only with the border.  So we'll use the same 
-   * function for each piece. 
-   * 
-   * The only slightly different piece is the larger circle of the gear, 
-   * as we will have to draw the teeth.
-   */
-  function renderGearPiece({
-    center,
-    radius,
-    fillColor,
-    strokeColor,
-    numberOfTeeth,
-  }) {
-    const { gl } = webglVars.current;
-    const { TRIANGLE_STRIP, POINTS, TRIANGLES } = gl;
-    const coords = getCircleCoords(gl, center[0], center[1], radius);
-
-    if (fillColor) drawShape(coords, fillColor, TRIANGLE_STRIP);
-    if (strokeColor) drawShape(coords, strokeColor, POINTS);
-    if (numberOfTeeth) {
-      drawShape(
-        getGearTeeth(gl, center, radius, numberOfTeeth),
-        fillColor,
-        TRIANGLES,
-      );
-    }
-  }
-
-  /**
-   * This function is in charge of drawing all the gears, giving it the 
-   * rotation on itself that it touches according to the angle calculated 
-   * in each interval.
-   */
-  function drawGears() {
-    const {
-      gl,
-      angles = [],
-      moveOriginLocation,
-      rotationLocation,
-      translationLocation,
-    } = webglVars.current;
-
-    gears.forEach((gear, index) => {
-      const [centerX, centerY] = gear.center;
-
-      gl.uniformMatrix3fv(
-        translationLocation,
-        false,
-        translation(centerX, centerY),
-      );
-      gl.uniformMatrix3fv(rotationLocation, false, rotation(angles[index]));
-      gl.uniformMatrix3fv(
-        moveOriginLocation,
-        false,
-        translation(-centerX, -centerY),
-      );
-      renderGearPiece(gear);
-      if (gear.children) gear.children.forEach(renderGearPiece);
-    });
-  }
-
-  /**
-   * Each piece of the gear, whether it is a toothed wheel, a painted 
-   * circle, or a rimmed circle, has a different shape. So this function 
-   * takes care of drawing each shape. Either a painted circle, which we 
-   * will do with triangles, or an unpainted circle that we will do 
-   * with points.
-   */
-  function drawShape(coords, color, drawingMode) {
-    const { gl, program } = webglVars.current;
-    const data = new Float32Array(coords);
-    const buffer = createAndBindBuffer(
-      gl,
-      gl.ARRAY_BUFFER,
-      gl.STATIC_DRAW,
-      data,
-    );
-
-    gl.useProgram(program);
-    linkGPUAndCPU(gl, { program, buffer, gpuVariable: 'position' });
-
-    const inputColor = gl.getUniformLocation(program, 'inputColor');
-    gl.uniform3fv(inputColor, color);
-    gl.drawArrays(drawingMode, 0, coords.length / 2);
-  }
-
-  /**
-   * This function is in charge of rendering each frame. When it is finished, 
-   * it calls itself again.
-   */
-  function run() {
-    gears.forEach((gear, index) => {
-      const direction = gear.direction === 'clockwise' ? 1 : -1;
-      const step = direction * (rotationStep / gear.numberOfTeeth);
-      const angle = webglVars.current.angles[index];
-
-      webglVars.current.angles[index] = (angle + step) % 360;
-    });
-    drawGears();
-
-    // Render next frame
-    window.requestAnimationFrame(run)
-  }
-
-  // As output of the hook we return the reference to the canvas, 
+  // As output of the hook we return the reference to the canvas,
   // because it will be mandatory to link it to the HTML canvas.
   return canvasRef;
 }
